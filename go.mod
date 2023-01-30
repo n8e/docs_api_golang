30563fcc5324674766a97a59e611257ed1ee72ac
Nate Martin <n8emar10@gmail.com>
2023-01-30 12:05:26 +0300

    chore: restrict routes that need authorization token

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/go-playground/validator/v10 v10.11.1
 	github.com/gofiber/fiber/v2 v2.41.0
+	github.com/gofiber/jwt/v3 v3.3.5
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/joho/godotenv v1.4.0
 	go.mongodb.org/mongo-driver v1.11.1
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"docs_api_golang/configs"
-	"docs_api_golang/routes"
+	"docs_api_golang/controllers"
 
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
 )
 
 func main() {
@@ -13,8 +14,24 @@ func main() {
 	// run database
 	configs.ConnectDB()
 
+	app.Post("/auth", controllers.Auth)
+	app.Post("/users", controllers.CreateUser)
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte(configs.EnvVariable("JWT_SECRET_KEY")),
+	}))
+
 	// routes
-	routes.AllRoutes(app)
+	app.Get("/users", controllers.GetUsers)
+	app.Get("/users/:id", controllers.GetUser)
+	app.Put("/users/:id", controllers.UpdateUser)
+	app.Delete("/users/:id", controllers.DeleteUser)
+
+	app.Get("/documents", controllers.GetDocuments)
+	app.Get("/documents/:id?", controllers.GetDocument)
+	app.Put("/documents/:id", controllers.UpdateDocument)
+	app.Post("/documents", controllers.CreateDocument)
+	app.Delete("/documents/:id", controllers.DeleteDocument)
 
 	app.Listen(":8000")
 }
